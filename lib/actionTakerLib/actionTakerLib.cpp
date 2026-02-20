636b3f74abe98aa0777ebf1d856d9c439f316f30
TheRadioactiveGoose <161170469+TheRadioactiveGoose@users.noreply.github.com>
2026-02-20 13:30:24 -0500

    changes: removed PAIR from command list, collapsed into seperate state. added commandAction(); to commandlib, which uses switch from commandParse to call commands. added actionTakerLib as a new custom local library, and initialized the header file. added placeholder functions to represent what the future functions will be for commandAction(); to use.

---
+++ lib/actionTakerLib/actionTakerLib.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+
+void cameraOn ()
+{
+
+}
+
+void cameraOff ()
+{
+
+}
+
+void cameraConnect ()
+{
+
+}
+
+void calibrateBMP ()
+{
+
+}
+
+void setPressureBMP ()
+{
+
+}
+
+void calibrateBNO ()
+{
+
+}
+
+void FGCon ()
+{
+
+}
+
+void testFGC ()
+{
+
+}
+
+void FGCoff ()
+{
+
+}
+
+void ABORT ()
+{
+
+}
+
+void Pair ()
+{
+
+}
---
+++ lib/actionTakerLib/actionTakerLib.h
@@ -0,0 +1,18 @@
+#ifndef actionTakerLib_H
+#define actionTakerLib_H
+
+#include <Arduino.h>
+
+void cameraOn ();
+void cameraOff ();
+void cameraConnect ();
+void calibrateBMP ();
+void setPressureBMP ();
+void calibrateBNO ();
+void FGCon ();
+void testFGC ();
+void FGCoff ();
+void ABORT ();
+void Pair ();
+
+#endif
