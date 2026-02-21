e3c2e7777ef400c12c658fb8ddbf1db040e5754c
TheRadioactiveGoose <awsomepro145@gmail.com>
2026-02-21 17:02:04 -0500

    implemented setPressureBMP, calibrateBNO, and fixed calibrateBMP, while also updating commandLib to match, and excluding all camera and Flight Guidance Computer commands from release 1 list.

--- lib/CommandLib/CommandLib.h
+++ lib/CommandLib/CommandLib.h
@@ -2,9 +2,14 @@
 #define CommandLib_H
 
 #include <Arduino.h>
+#include "Adafruit_BMP3XX.h"
+#include "Wire.h"
+#include "Adafruit_Sensor.h"
+#include "SPI.h"
+#include "Adafruit_BNO055.h"
 #include "actionTakerLib.h"
 
 void commandParse(String command, bool isCommand, int commandKey);
-void commandAction(int commandKey, char commandBuffer[16], bool testResult);
+void commandAction(int commandKey, char commandBuffer[16], bool testResult, Adafruit_BMP3XX bmp, Adafruit_BNO055 bno);
 
 #endif
--- lib/actionTakerLib/actionTakerLib.cpp
+++ lib/actionTakerLib/actionTakerLib.cpp
@@ -1,22 +1,30 @@
 #include <Arduino.h>
+#include <string.h>
 #include "Adafruit_BMP3XX.h"
 #include "Wire.h"
 #include "Adafruit_Sensor.h"
 #include "SPI.h"
+#include "Adafruit_BNO055.h"
 
 void cameraOn ()
 {
 //to be implemented later
+//DOESNT NEED TO BE IMPLEMENTED FOR FIRST FLIGHT // CAMERA MANUALLY ACTIVATED
+return;
 }
 
 void cameraOff ()
 {
 //to be implemented later
+//DOESNT NEED TO BE IMPLEMENTED FOR FIRST FLIGHT // CAMERA MANUALLY ACTIVATED
+return;
 }
 
 void cameraConnect ()
 {
 //to be implemented later
+//DOESNT NEED TO BE IMPLEMENTED FOR FIRST FLIGHT // CAMERA MANUALLY ACTIVATED
+return;
 }
 
 void calibrateBMP (Adafruit_BMP3XX bmp, int seaLevelPressure, bool testResult)
@@ -26,36 +34,65 @@ void calibrateBMP (Adafruit_BMP3XX bmp, int seaLevelPressure, bool testResult)
     if (calculatedAltitude != bmp.readAltitude(seaLevelPressure))
     {
         testResult = false;
+        return;
     }
     else
     {
         testResult = true;
+        return;
     }
 }
 
-void setPressureBMP (char commandBuffer[16], int seaLevelPressure)
+void setPressureBMP (Adafruit_BMP3XX bmp, char commandBuffer[16], int seaLevelPressure)
 {
-    memcpy(&seaLevelPressure, &commandBuffer[4], 4); //figure out the length of how many characters the sea level pressure is and change the 4 to that number, and also make sure it is divided properly to match that
+    #define pressureDataLength 8
+    #define dataStartLocation 5
+    char pressData[9] = {0}; // preps an empty character array to hold just the data, to make atoi easy
+    strncpy(pressData, commandBuffer + dataStartLocation, pressureDataLength); // places the selected locations from commandBuffer into pressData
+    pressData[9] = '\0'; // adds a null pointer at the end of pressData to make it a c string for atoi
+    seaLevelPressure = atoi(pressData); // places the characters of pressData into seaLevelPressure as the new seaLevelPressure
 }
 
-void calibrateBNO ()
+void calibrateBNO (Adafruit_BNO055 bno, bool testResult)
 {
+  // Get the four calibration values (0..3)
+  // Any sensor data reporting 0 should be ignored, 3 means 'fully calibrated"
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
 
+  /* The data should be ignored until the system calibration is > 0 */
+  if (!system)
+  {
+    testResult = false;
+    return;
+  }
+  else
+  {
+    testResult = true;
+    return;
+  }
 }
 
 void FGCon ()
 {
 //to be implemented when IntercomLib is done
+//DOESNT NEED TO BE IMPLEMENTED FOR FIRST FLIGHT // FGC NOT FLYING
+return;
 }
 
 void testFGC (char commandBuffer[16], bool testResult)
 {
 //to be implemented when IntercomLib is done
+//DOESNT NEED TO BE IMPLEMENTED FOR FIRST FLIGHT // FGC NOT FLYING
+return;
 }
 
 void FGCoff ()
 {
 //to be implemented when IntercomLib is done
+//DOESNT NEED TO BE IMPLEMENTED FOR FIRST FLIGHT // FGC NOT FLYING
+return;
 }
 
 void ABORT ()
