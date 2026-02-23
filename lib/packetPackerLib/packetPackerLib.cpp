04b8ac90341135116301f6ea0d17a2d54014ecaf
TheRadioactiveGoose <awsomepro145@gmail.com>
2026-02-22 23:31:29 -0500

    added implementations for IMU data, including accelerometer and euler angles, paused work before angular velocity and acceleration.

--- lib/packetPackerLib/packetPackerLib.cpp
+++ lib/packetPackerLib/packetPackerLib.cpp
@@ -6,7 +6,7 @@
 
 String datastring;
 
-void packNavigationalBufferPreKalman (Adafruit_BMP3XX bmp, Adafruit_BNO055 bno, Adafruit_GPS GPS, char navPacket[82], int seaLevelPressure)
+void packNavigationalBufferPreKalman (Adafruit_BMP3XX bmp, Adafruit_BNO055 bno, Adafruit_GPS GPS, char navPacket[82], int seaLevelPressure, int prev_euler_x, int prev_euler_y, int prev_euler_z)
 {
 
     GPS.read();
@@ -23,6 +23,20 @@ void packNavigationalBufferPreKalman (Adafruit_BMP3XX bmp, Adafruit_BNO055 bno, 
     datastring = String(bmp.readAltitude(seaLevelPressure));
     datastring.toCharArray(&navPacket[21], 7);
 
-    datastring = String(); //gotta figure out the BNO055
+    imu::Vector<3> linearaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+    datastring = String(linearaccel.z()); //gotta figure out the BNO055
     datastring.toCharArray(&navPacket[28], 5);
+
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+
+    datastring = String(euler.x());
+    datastring.toCharArray(&navPacket[33], 6);
+
+    datastring = String(euler.y());
+    datastring.toCharArray(&navPacket[39], 6);
+
+    datastring = String(euler.z());
+    datastring.toCharArray(&navPacket[45], 6);
+
 }
